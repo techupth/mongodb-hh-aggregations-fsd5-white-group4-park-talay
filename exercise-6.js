a54098831349b6ba07915ecdf64c91856d5479cc
Pacharadanai <pacharadanaipinake@gmail.com>
2024-01-11 13:59:19 +0700

    feat: add completed exercise

--- exercise-6.js
+++ exercise-6.js
@@ -1,1 +1,18 @@
 // Paste the complete MQL here
+db.pizzaOrders.aggregate([
+    {
+        $group: {
+            _id: {
+                month: { $month: "$created_at" },
+                year: { $year: "$created_at" },
+            },
+            total_sales: { $sum: "$total_price" },
+        },
+    },
+    {
+        $match: {
+            "_id.year": 2021,
+            "_id.month": 7,
+        },
+    },
+]);
--- exercise-7.js
+++ exercise-7.js
@@ -1,1 +1,23 @@
 // Paste the complete MQL here
+db.pizzaOrders.aggregate([
+    {
+        $group: {
+            _id: {
+                month: { $month: "$created_at" },
+                year: { $year: "$created_at" },
+            },
+            total_sales: { $sum: "$total_price" },
+        },
+    },
+    {
+        $match: {
+            "_id.year": 2021,
+        },
+    },
+    {
+        $sort: { total_sales: 1 },
+    },
+    {
+        $limit: 1,
+    },
+]);
