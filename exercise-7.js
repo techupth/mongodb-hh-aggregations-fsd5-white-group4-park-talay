ccaabc6e4173978ec7775e5e52bf68bd34e64add
Park <tanagrit.sornchaiyagorn@gmail.com>
2024-01-11 16:52:45 +0700

    query: query follow requirement ex1-7

--- exercise-7.js
+++ exercise-7.js
@@ -1,1 +1,21 @@
 // Paste the complete MQL here
+// db.pizzaOrders.aggregate([
+//   {
+//     $group: {
+//       _id: { month: { $month: "$created_at" }, year: { $year: "$created_at" } },
+//       total_price_by_month: { $sum: "$total_price" },
+//     },
+//   },
+//   {
+//     $sort: { "_id.year": -1, "_id.month": -1 },
+//   },
+//   {
+//     $match: { "_id.year": 2021 },
+//   },
+//   {
+//     $sort: { total_price_by_month: 1 },
+//   },
+//   {
+//     $limit: 1,
+//   },
+// ]);
